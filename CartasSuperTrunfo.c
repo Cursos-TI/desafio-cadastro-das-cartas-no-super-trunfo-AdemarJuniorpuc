003591a43c1f3c473591a02dda31f93aca37e99c
AdemarJuniorpuc <126532699+AdemarJuniorpuc@users.noreply.github.com>
2025-02-24 12:53:10 +0000

    criei o prinft

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,47 @@
 //Teste larissa
 
 int main() {
+    int carta = 01;
+    char codigo[20] = "A01";
+    char estado[20] = "Minas Gerais";
+    char cidade[20] = "Belo horizonte";
+    float populacao = 6.300410;
+    double area= 331.354;
+    float pib = 284;
+    int numero = 50;
+    
+ 
+    printf("carta: %dº Carta\n", carta);
+    printf("codigo: %s\n", codigo);
+    printf("estado: %s\n", estado);
+    printf("cidade: %s\n", cidade);
+    printf("população: %f de habitantes\n", populacao);
+    printf("Area: %.3f quilômetros quadrados\n", area);
+    printf("pib: %.0f bilhões \n", pib);
+    printf("numero: %d numeros de pontos turisticos\n", numero);
+
+    printf("\n\n");
+
+    int carta1 = 02;
+    char codigo1[20] = "A02";
+    char estado1[20] = "Minas Gerais";
+    char cidade1[20] = "Contagem";
+    float populacao1 = 621.865;
+    double area1= 194.746;
+    float pib1= 29.558;
+    int numero1 = 30;
+
+
+    printf("carta: %dº Carta\n", carta1);
+    printf("codigo: %s\n", codigo1);
+    printf("estado: %s\n", estado1);
+    printf("cidade: %s\n", cidade1);
+    printf("população: %.3f de habitantes\n", populacao1);
+    printf("Area: %.3f quilômetros quadrados\n", area1);
+    printf("pib: %.3f bilhões \n", pib1);
+    printf("numero: %d numeros de pontos turisticos\n", numero1);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
